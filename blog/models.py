70adbc9c4ca62faae72f38eb11bbede0992abe02
Kevin Konczak <konczakkevin@gmail.com>
2023-02-07 14:29:52 +0900

    Added a blog homepage model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
-# Create your models here.
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel('intro')]
