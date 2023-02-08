bc2d6c3054378da4fc3df4931bca74883a086689
Kevin Konczak <konczakkevin@gmail.com>
2023-02-08 16:00:50 +0900

    Added new model for categories

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.search import index
+from wagtail.snippets.models import register_snippet
 
 
 class BlogIndexPage(Page):
@@ -80,3 +81,24 @@ class BlogTagIndexPage(Page):
         context = super().get_context(request)
         context['blogpages'] = blogpages
         return context
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=250)
+    icon = models.ForeignKey('wagtailimages.Image',
+                             null=True,
+                             blank=True,
+                             on_delete=models.SET_NULL,
+                             related_name='+')
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
